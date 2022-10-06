797e692500d0642ed915c9725f376f8a77ce25fa
NidhalNouma <nidhal.nouma.0@gmail.com>
2022-10-06 22:28:26 +0100

    disable redirect to landing page

--- utils/constant.js
+++ utils/constant.js
@@ -12,7 +12,7 @@ export const firebaseConfig = {
   measurementId: "G-N59NRR3N6T",
 };
 
-export const landingUrl = process.env.NEXT_PUBLIC_LANDING_URL || "/";
+export const landingUrl = "/"; //process.env.NEXT_PUBLIC_LANDING_URL || "/";
 
 export function WebhhokURL() {
   return window.location.protocol + "//" + window.location.host + "/api/wh/";
