5cbb619dc72bc8b8d993d945eda71f2870105a1b
NidhalNouma <nidhal.nouma.0@gmail.com>
2022-11-15 23:31:03 +0100

    update EA to send all orders, change chart layout 1

--- pages/api/accounts/mt4/index.js
+++ pages/api/accounts/mt4/index.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         accountStartBalance,
         "MT4"
       );
-      if (r)
+      if (r.id)
         return res.status(200).json({ id: r.id, exist: r.exist, done: true });
     }
   }
