aeebca90570b4ce70c9fc8fed7f29e4d2f267773
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2024-04-04 05:19:13 +0100

    Adding emails to the allowed email list

--- utils/allowedEmails.js
+++ utils/allowedEmails.js
@@ -2,5 +2,6 @@ export const emailsList = [
   "nidhal.nouma.0@gmail.com",
   "nidhal.nouma.1@gmail.com",
   "automatedtrader@gmail.com",
-  "chrisp@gmail.com",
+  "Melaniekers@gmail.com",
+  "Cheatcodefx@gmail.com",
 ];
