a163ecd4cff2b4d89c9267aa5375ce027f5223a6
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2024-10-13 12:36:04 +0100

    trying to fix sellix hook

--- frontend/pages/api/sellix/hook.js
+++ frontend/pages/api/sellix/hook.js
@@ -4,7 +4,7 @@ import crypto from "crypto";
 const secret = process.env.NEXT_PUBLIC_SELLIX_WEBHOOK_SECRET;
 
 export default async function handler(req, res) {
-  const headerSignature = req.headers["x-sellix-unescaped-signature"];
+  const headerSignature = req.headers["X-Sellix-Unescaped-Signature"];
   const event = req.headers["X-Sellix-Event"];
   const payload = req.body;
 
@@ -13,17 +13,24 @@ export default async function handler(req, res) {
     .update(payload)
     .digest("hex");
 
+  console.log(req.headers);
+  console.log(headerSignature);
   console.log(event);
-  console.log(payload);
-  if (
-    crypto.timingSafeEqual(
-      Buffer.from(signature),
-      Buffer.from(headerSignature, "utf-8")
-    )
-  ) {
-    // handle valid webhook
-  } else {
-    // invalid webhook
+
+  try {
+    if (
+      crypto.timingSafeEqual(
+        Buffer.from(signature),
+        Buffer.from(headerSignature, "utf-8")
+      )
+    ) {
+      // handle valid webhook
+    } else {
+      // invalid webhook
+    }
+  } catch (e) {
+    console.error(e.message);
+    res.status(400).json({ error: e.message, headers: req.headers });
   }
 
   return res.status(200).json({ done: false });
